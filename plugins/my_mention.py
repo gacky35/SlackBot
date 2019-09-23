0dbd71e09245ba8a3eda929a2a10474b8eb24495
gacky35 <s17t258@stu.kagawa-u.ac.jp>
2019-09-23 20:25:54 +0900

    I wrote a program which uses slackclient in a separate file

--- plugins/my_mention.py
+++ plugins/my_mention.py
@@ -1,19 +1,7 @@
 from slackbot.bot import respond_to
 from slackbot.bot import listen_to
 from slackbot.bot import default_reply
-import slack
-
-client_token = 'xoxp-761305137745-754949468914-767736312661-9f26e5c1e6c2282046548a21e303299e'
-client = slack.WebClient(token=client_token)
-channel_id = 'CND8Z4T3K'
-
-def get_message(client, channel_id, thread_ts):
-    response = client.api_call(
-            "channels.replies",
-            channel = channel_id,
-            thread_ts=thread_ts
-            )
-    return response
+from . import subMethod
 
 @respond_to('@hoge\s.*')
 def reply_to_thread(message):
@@ -22,8 +10,5 @@ def reply_to_thread(message):
 
 @respond_to('count')
 def count_up_reaction(message):
-    response = client.channels_replies(
-            channel=channel_id, 
-            thread_ts=message.thread_ts
-            )
+    response = subMethod.get_message(message.thread_ts)
     print(response['messages'][0])
