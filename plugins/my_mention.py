3fd8b823fd1ec343e2d538e8d22b30021a853f08
gacky35 <s17t258@stu.kagawa-u.ac.jp>
2019-09-25 19:35:34 +0900

    prototype of usersgroup management

--- plugins/my_mention.py
+++ plugins/my_mention.py
@@ -3,9 +3,20 @@ from slackbot.bot import listen_to
 from slackbot.bot import default_reply
 from . import subMethod
 
-@respond_to('@hoge\s.*')
-def reply_to_thread(message):
-    message.send('<@kayte035> please check it out!', 
+usergroup = []
+
+@respond_to('@[a-zA-Z0-9]+\s([\s\S]*)')
+def reply_to_thread(message, text):
+    mention = message.body['text'].split(' ')[0].strip('@')
+    for dictionary in usergroup:
+        if dictionary['usergroup_name'] == mention:
+            mention_dict = dictionary
+            break
+    sentence = ""
+    for member in mention_dict['member']:
+        sentence = sentence + "<@" + member + "> "
+    sentence = sentence + "\n" + text
+    message.send(sentence, 
             thread_ts=message.thread_ts)
 
 @respond_to('count')
@@ -21,3 +32,18 @@ def count_up_reaction(message):
             sentence = sentence + "<@" + user + "> "
         sentence = sentence + "\n"
     message.direct_reply(sentence)
+
+@respond_to('create\s([a-zA-Z0-9]*)\s([a-zA-Z0-9,]*)')
+def create_usergroup(message, usergroup_name, member):
+    member_list = subMethod.get_member()['members']
+    data = {}
+    data['usergroup_name'] = usergroup_name
+    member_name = member.split(',')
+    member_id = []
+    for ml in member_list:
+        for mn in member_name:
+            if ml['name'] == mn or ml['real_name'] == mn:
+                member_id.append(ml['id'])
+    data['member'] = member_id
+    usergroup.append(data)
+    message.send('OK')
