b1eb0c3e9c75fef7b8857623765d0d9223b2516d
gacky35 <s17t258@stu.kagawa-u.ac.jp>
2019-10-13 14:59:32 +0900

    modified error message

--- plugins/my_mention.py
+++ plugins/my_mention.py
@@ -14,7 +14,7 @@ def reply_to_thread(message, text):
             mention_dict = dictionary
             break
     if len(mention_dict) == 0:
-        message.send(mention + ' is not exist')
+        message.send('`' + mention + ' is not exist`')
         return
     sentence = ""
     for member in mention_dict['member']:
@@ -76,7 +76,7 @@ def create_usergroup(message, usergroup_name, member):
 #    data['member'] = subMethod.username_to_userid(member_list, member_name)
     usergroup.append(data)
     subMethod.set_usergroup_list(usergroup)
-    message.send('OK')
+    message.send('Created a usergroup')
 
 @respond_to('add\s([a-zA-Z0-9]*)\s([a-zA-Z0-9,]*)')
 def add_member(message, usergroup_name, member):
@@ -93,7 +93,7 @@ def add_member(message, usergroup_name, member):
         if mn not in usergroup_member:
             add_member_name.append(mn)
         else:
-            message.send(mn + ' already belongs')
+            message.send("`" + mn + ' already belongs`')
     member_id = []
     ml_id = [ml['id'] for ml in member_list]
     ml_name = [ml['name'] for ml in member_list]
@@ -104,24 +104,24 @@ def add_member(message, usergroup_name, member):
         elif mn in ml_rname:
             member_id.append(ml_id[ml_rname.index(mn)])
         else:
-            message.send(mn + " is not in this channel")
+            message.send("`" + mn + " is not in this channel`")
     #member_id = subMethod.username_to_userid(member_list, member_name)
     if len(member_id) == 0:
-        message.send("No one will add")
+        message.send("`No one will add`")
         return
     for usergroup_dict in usergroup:
         if usergroup_dict['usergroup_name'] == usergroup_name:
             usergroup_dict['member'].extend(member_id)
             break
     subMethod.set_usergroup_list(usergroup)
-    message.send('OK')
+    message.send('Added some member')
 
 @respond_to('delete\s([a-zA-Z0-9]*)\s([a-zA-Z0-9,]*)')
 def delete_member(message, usergroup_name, member):
     usergroup = subMethod.get_usergroup_list()
     usergroup_name_list = [usergroup_dict['usergroup_name'] for usergroup_dict in usergroup]
     if usergroup_name not in usergroup_name_list:
-        message.send(usergroup_name + " is not exist")
+        message.send("`" + usergroup_name + " is not exist`\n> type `@starbot list` and check usergroup_name")
         return
     member_list = subMethod.get_member()['members']
     member_name = member.split(',')
@@ -136,27 +136,27 @@ def delete_member(message, usergroup_name, member):
         elif mn in ml_rname:
             member_id.append(ml_id[ml_rname.index(mn)])
         else:
-            message.send(mn + " is not in this channel")
+            message.send("`" + mn + " is not in this channel`")
     if len(member_id) == 0:
-        message.send("No one will delete")
+        message.send("`No one will delete`")
         return
     for usergroup_dict in usergroup:
         if usergroup_dict['usergroup_name'] == usergroup_name:
             for mi in member_id:
                 if mi not in usergroup_dict['member']:
-                    message.send(ml_name[ml_id.index(mi)] + " doesn't belong to this")
+                    message.send("`" + ml_name[ml_id.index(mi)] + " doesn't belong to this`")
                 else:
                     usergroup_dict['member'].remove(mi)
             break
     subMethod.set_usergroup_list(usergroup)
-    message.send('OK. deleted member')
+    message.send('Deleted some member')
 
 @respond_to('delete_usergroup\s([a-zA-Z0-9]*)')
 def delete_usergroup(message, usergroup_name):
     usergroup = subMethod.get_usergroup_list()
     usergroup_name_list = [x['usergroup_name'] for x in usergroup]
     if usergroup_name not in usergroup_name_list:
-        message.send(usergroup_name + ' is not exist.')
+        message.send("`" + usergroup_name + ' is not exist.`\n> type `@starbot list` and check usergroup_name')
         return
     new_usergroup = []
     for usergroup_dict in usergroup:
@@ -164,14 +164,14 @@ def delete_usergroup(message, usergroup_name):
             continue
         new_usergroup.append(usergroup_dict)
     subMethod.set_usergroup_list(new_usergroup)
-    message.send('OK. deleted usergroup')
+    message.send('Deleted a usergroup')
 
 @respond_to('rename\s([a-zA-Z0-9]*)\s([a-zA-Z0-9]*)')
 def rename_usergroup(message, usergroup_name, new_usergroup_name):
     usergroups = subMethod.get_usergroup_list()
     usergroup_name_list = [x['usergroup_name'] for x in usergroups]
     if usergroup_name not in usergroup_name_list:
-        message.send(usergroup_name + ' is not exist.')
+        message.send("`" + usergroup_name + ' is not exist.`')
         return
     if new_usergroup_name in usergroup_name_list:
         message.send("`" + new_usergroup_name + ' is exist.`\n> please choose another name')
@@ -181,7 +181,7 @@ def rename_usergroup(message, usergroup_name, new_usergroup_name):
             usergroup['usergroup_name'] = new_usergroup_name
             break
     subMethod.set_usergroup_list(usergroups)
-    message.send('OK. rename usergroup')
+    message.send('Renamed a usergroup')
 
 @respond_to('list')
 def show_usergroup_list(message):
